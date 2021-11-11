83564c10215b685997d950a6a0256ebbbd674f7e
lukefrogger <lukefrogger@gmail.com>
2021-11-11 12:09:08 -0600

    updated package

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Button", {
+Object.defineProperty(exports, "FetchForm", {
   enumerable: true,
   get: function get() {
-    return _button.default;
+    return _FetchForm.default;
   }
 });
 
-var _button = _interopRequireDefault(require("./button"));
+var _FetchForm = _interopRequireDefault(require("./exports/components/FetchForm"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
